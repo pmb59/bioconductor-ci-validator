0dad767d92ae398135b0ea445a978999b521bfd8
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-05-14 00:31:05 +0100

    Update build_ExpressionAtlas.sh

--- build_ExpressionAtlas.sh
+++ build_ExpressionAtlas.sh
@@ -2,4 +2,6 @@
 
 git clone https://github.com/ebi-gene-expression-group/bioconductor-ExpressionAtlas
 
-R CMD build
+R CMD build bioconductor-ExpressionAtlas
+
+R CMD check ExpressionAtlas_*.tar.gz
