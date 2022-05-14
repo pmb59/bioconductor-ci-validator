76c4e64ee7ac0296a7f0a89ba739ae8fc6a6eebe
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-05-14 14:21:42 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pkg_to_build=$( cat packages )
+# if bioc tagged, rm prefix
+prefix="bioconductor-"
 
 for p in $pkg_to_build ; do
 
@@ -9,29 +11,26 @@ for p in $pkg_to_build ; do
   git clone $p
   
   pkg=$( awk -F/ '{print $NF}' <<< $p )
-  
-  #echo $pkg
+  # remove prefix
+  pkgsed=$( echo "$pkg" | sed -e "s/^$prefix//" )
 
   Rscript --vanilla install_dependencies.r $pkg 2>&1 >/dev/null
 
   R CMD build $pkg --no-manual --no-build-vignettes 
   if [ $? -ne 0 ]; then
-    echo $pkg"  ERROR: Failed R CMD build." >> summary.txt
+    echo $pkgsed"  ERROR: Failed R CMD build." >> summary.txt
     exit 1
   else
-    echo $pkg"  build OK." >> summary.txt
+    echo $pkgsed"  build OK." >> summary.txt
   fi
   
-  # if bioc tagged, rm prefix
-  prefix="bioconductor-"
-  pkg=$( echo "$pkg" | sed -e "s/^$prefix//" )
 
-  R CMD check ${pkg}_*.tar.gz --no-manual --no-build-vignettes 
+  R CMD check ${pkgsed}_*.tar.gz --no-manual --no-build-vignettes 
   if [ $? -ne 0 ]; then
-    echo $pkg"  ERROR: R CMD check." >> summary.txt
+    echo $pkgsed"  ERROR: R CMD check." >> summary.txt
     exit 1
   else
-    echo $pkg"  check OK."  >> summary.txt
+    echo $pkgsed"  check OK."  >> summary.txt
   fi
   
   # include other checks, size, tarball, etc...
