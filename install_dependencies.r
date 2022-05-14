1b8805e012a364a753e77356945a674a2f7cb182
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-05-14 14:18:07 +0100

    [ci skip]

--- install_dependencies.r
+++ install_dependencies.r
@@ -4,14 +4,13 @@ if (length(args)!=1) {
   stop("one argument must be supplied", call.=FALSE)
 } 
 
-if (!require("remotes")) install.packages("remotes")
-
+if (!require("remotes")){
+  install.packages("remotes")
+}
 #if (!require("devtools")) install.packages("devtools")
 #if (!require("tinytex")) install.packages('tinytex')
 #library("tinytex")
 #tinytex::install_tinytex()  # install TinyTeX
 
-
 library(remotes)
-install_deps(paste0('./',args[1]), dependencies=TRUE, quiet=FALSE) 
-
+install_deps(paste0('./',args[1]), dependencies=TRUE, quiet=TRUE) 
