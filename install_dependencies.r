c4376d2bfe8f1bcef4135575dbded28325319203
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-05-14 01:13:23 +0100

    Update install_dependencies.r

--- install_dependencies.r
+++ install_dependencies.r
@@ -2,3 +2,7 @@ if (!require("remotes")) install.packages("remotes")
 
 library(remotes)
 install_deps('./bioconductor-ExpressionAtlas') 
+
+if (!require("knitr")) install.packages("knitr")
+if (!require("testthat")) install.packages("testthat")
+if (!require("rmarkdown")) install.packages("rmarkdown)
